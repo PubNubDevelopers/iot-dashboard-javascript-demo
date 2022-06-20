8949043059c6026aa5a2f34990779679cfa79944
darryn.campbell <darryn.campbell@pubnub.com>
2022-06-20 09:49:59 +0100

    stub for demo interface

---
+++ public/js/demo/demo_interface.js
@@ -0,0 +1,14 @@
+/**
+ * Interface between the app client code and the NodeJS endpoint that will send the message to the interactive demo
+ */
+
+function actionCompleted (args) {
+  //args: {
+  //    action:string,
+  //    blockDuplicateCalls:boolean,
+  //    debug:boolean
+  //}) {
+
+
+  return
+}
