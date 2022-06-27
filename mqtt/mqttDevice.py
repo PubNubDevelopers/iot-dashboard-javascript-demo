27e4c97cdc2f5d2150ec8d2873f20b03ba8f1986
darryn.campbell <darryn.campbell@pubnub.com>
2022-06-27 10:04:11 +0100

    Tidying and bug fixes

--- mqtt/mqttDevice.py
+++ mqtt/mqttDevice.py
@@ -1,13 +1,16 @@
+# Sample MQTT client - proof of concept
+# MQTT does not support file transfer.  Though not supported in this sample file, in production you could do this using the PubNub SDK, e.g. the Python SDK if you were extending this sample.
+
 import paho.mqtt.client as mqtt
 from threading import Timer
 import json
+import random
 
 publish_key = ""    # YOUR PUBNUB PUBLISH KEY HERE
 subscribe_key = ""  # YOUR PUBNUB SUBSCRIBE KEY HERE
 client_id = "sim_py"
 channel_name = "device/" + client_id
 channel_name2 = "device." + client_id
-channel_name3 = "device/#"
 channel_name4 = "mqtt_data_channel"
 device_name = "MQTT Python Device"
 sensor_frequency = 5000
@@ -16,6 +19,8 @@ latitude = "51.4037"
 longitude = "-0.3376"
 firmware_version = "v1.0"
 client = mqtt.Client(client_id=publish_key + "/" + subscribe_key + "/" + client_id)
+timer_post = Timer(1000, ())
+timer_connect = Timer(1000, ())
 
 def debounce(wait_time):
     """
@@ -43,7 +48,6 @@ def debounce(wait_time):
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
-    #client.subscribe("device/sim_py")  # Get the action:reboot this way
     client.subscribe(channel_name)
     client.subscribe(channel_name4)
     provision_device()
@@ -54,28 +58,33 @@ def on_disconnect(client, userdata, rc):
 
 @debounce(0.1)
 def on_message(client, userdata, msg):
-    print("onMsg " + msg.topic + " " + str(msg.payload) + " " + str(userdata))
+    #print("onMsg " + msg.topic + " " + str(msg.payload) + " " + str(userdata))
     if str(msg.payload).find('"action":"reboot"') != -1:
         print("DEVICE IS REBOOTING")
         client.unsubscribe(channel_name)
         client.unsubscribe(channel_name4)
         client.disconnect()
-        r = Timer(5.0, connect)
-        r.start()
+        timer_post.cancel()
+        global timer_connect
+        timer_connect = Timer(7.0, connect)
+        timer_connect.start()
     elif str(msg.payload).find('"action":"update"') != -1:
-        print ("Updating parameters")
+        print ("UPDATING PARAMETERS")
         attr = json.loads(msg.payload)
         global sensor_frequency 
         sensor_frequency = int(attr["params"]["sensorUpdateFrequency"])
         global device_name 
         device_name = attr["params"]["deviceName"]
+        timer_post.cancel()
+        post_message_loop()
     
 
 def post_message_loop():
-    msgStr = '{"mqtt_device_id": "' + client_id + '", "lat": "' + latitude + '", "long": "' + longitude + '", "friendly_name": "' + device_name + '", "firmware_version": "' + firmware_version + '", "sensors": [{"sensor_name": "Refrigerator Temperature", "sensor_type": "Temperature", "sensor_update_frequency": "' + str(sensor_frequency) + '", "sensor_value": "' + str(sensor_value) + '", "sensor_units": "°c"}]}'
+    msgStr = '{"mqtt_device_id": "' + client_id + '", "lat": "' + latitude + '", "long": "' + longitude + '", "friendly_name": "' + device_name + '", "firmware_version": "' + firmware_version + '", "sensors": [{"sensor_name": "Refrigerator Temperature", "sensor_type": "Temperature", "sensor_update_frequency": "' + str(sensor_frequency) + '", "sensor_value": "' + str(sensor_value + random.uniform(0.1, 0.4)) + '", "sensor_units": "°c"}]}'
     client.publish(channel_name4, msgStr)
-    r = Timer(sensor_frequency / 1000, post_message_loop)
-    r.start()
+    global timer_post
+    timer_post = Timer(sensor_frequency / 1000, post_message_loop)
+    timer_post.start()
 
 @debounce(0.1)
 def provision_device():
@@ -91,12 +100,6 @@ def connect():
     client.on_message = on_message
     client.connect("mqtt.pndsn.com", 1883, 6000)
     client.loop_forever()
-    print("Connect called")
 
 
-client.on_connect = on_connect
-client.on_disconnect = on_disconnect
-client.on_message = on_message
-client.connect("mqtt.pndsn.com", 1883, 6000)
-client.loop_forever()
-
+connect()
